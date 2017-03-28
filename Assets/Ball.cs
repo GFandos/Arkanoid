4d31ca49e8533f9248adc26d26fc7dc05c8bc5fe
GFandos <gerard.fandos@gmail.com>
2017-03-28 17:38:30 +0200

    Base game done.

---
+++ Assets/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 200f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        Debug.Log("Game starts");
+    }
+
+    float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        Debug.Log("Collision");
+
+        if (col.gameObject.name == "racket")
+        {
+            Debug.Log("Collision on racket");
+
+            float x = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.x);
+
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * (speed * 1.2f);
+        }
+    }
+	
+}
+
+
